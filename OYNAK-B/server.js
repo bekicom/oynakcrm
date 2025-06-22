a40d35056032fc2e2081f096006c8e13a634c1fb
Jasur <j31548359@gmail.com>
2025-06-22 12:18:26 +0500

    vercel

--- OYNAK-B/server.js
+++ OYNAK-B/server.js
@@ -9,7 +9,7 @@ const app = express();
 // ✅ CORS middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend domeni
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
