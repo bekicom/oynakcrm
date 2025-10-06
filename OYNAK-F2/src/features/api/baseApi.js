8fc30864c2433a80f69c1470343767f7af74397d
Jasur <j31548359@gmail.com>
2025-10-06 11:59:10 +0500

    url

--- OYNAK-F2/src/features/api/baseApi.js
+++ OYNAK-F2/src/features/api/baseApi.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://oynakcrm.vercel.app/api",
-    baseUrl: "http://localhost:8080/api",
+    baseUrl: "https://oynakcrm.vercel.app/api",
+    // baseUrl: "http://localhost:8080/api",
   }),
   tagTypes: ["Product", "Sale", "Client", "Stats"],
   endpoints: () => ({}),
